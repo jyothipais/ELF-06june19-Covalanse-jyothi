c59398508987ff2b1c05293df3d5b10ca845533f
jyothipais <flaviyajyothi0114@gmail.com>
2019-06-26 09:38:32 +0530

    assignments

---
+++ CoreJava/src/jun10assignments/javaapp/starpatterns/Pattern6.java
@@ -0,0 +1,17 @@
+package com.covalense.javaapp.starpatterns;
+
+public class Pattern6 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 4; i++) {
+			for (int k = 4; k >= i; k--) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= (2 * i) - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+
+}
---
+++ CoreJava/src/jun10assignments/javaapp/starpatterns/Pattern9a.java
@@ -0,0 +1,18 @@
+package com.covalense.javaapp.starpatterns;
+
+public class Pattern9a {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				if((i==3&&j<=5)||j==3) {
+					System.out.print("*");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
