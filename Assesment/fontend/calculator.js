17bab793e13ca882c8d703459800b8b2081757b2
jyothipais <flaviyajyothi0114@gmail.com>
2019-07-22 13:56:51 +0530

    Assessment on CSS,HTML,BOOTSTRAP,JAVASCRIPT Done on 22/07/20119

---
+++ Assesment/fontend/calculator.js
@@ -0,0 +1,9 @@
+/* function for calculating the given values*/
+function calC(c) {
+    //form.panel.value is used to store the value given by the user
+    form.panel.value = form.panel.value + c;
+}
+function CE() {
+    //clearing the text field
+    form.panel.value = "";
+}
